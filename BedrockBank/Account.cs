85f78249a4fb3b5f97b13fad2194b24d80c75a8a
sarahmathews <s.marie.mathews@gmail.com>
2016-04-02 11:52:40 -0700

    Initial codebase - added variables, constructors

---
+++ BedrockBank/Account.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBank
+{
+
+    public enum AccountType
+    {
+        Savings,
+        Checking,
+        CD,
+        Loans
+    }
+    /// <summary>
+    /// This is about a bank account
+    /// </summary>
+    public class Account
+    {
+        #region Variables
+        private static int lastAccountNumber = 0;
+        #endregion
+
+        #region Properties
+        /// <summary>
+        /// Account number for the account
+        /// </summary>
+        public int AccountNumber { get; private set; }
+        /// <summary>
+        /// Name of account
+        /// </summary>
+        public string AccountName { get; set; }
+        public int SSN { get; set; }
+        public double Balance { get; private set; }
+        public AccountType TypeOfAccount { get; set; }
+        #endregion
+
+        #region Constructor
+        public Account()
+        {
+            /*go read the last account number from the static memory, take it's value 
+            increment by one and it will create the next number */
+            //AccountNumber = lastAccountNumber + 1;
+            //lastAccountNumber = lastAccountNumber + 1;
+            AccountNumber = ++lastAccountNumber;
+        }
+        #endregion
+
+        #region Methods
+        public double Deposit(double amount)
+        {
+            Balance += amount;
+            return Balance;
+        }
+        #endregion
+    }
+}
---
+++ BedrockBank/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BedrockBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Creating an instance of Account
+            var account1 = new Account();
+            account1.AccountName = "My Checking";
+            account1.TypeOfAccount = AccountType.Checking;
+            //account1.AccountNumber = 45566666;
+            //account1.Balance = 1000000000000000000; 
+            account1.Deposit(300.00);
+            Console.WriteLine("Account Name: {0}, Number: {1}, Type of Account: {2}, Balance: {3:c}", account1.AccountName,
+                account1.AccountNumber, account1.TypeOfAccount, account1.Balance);
+
+            var account2 = new Account();
+            account2.AccountName = "My Savings";
+            account2.TypeOfAccount = AccountType.Savings;
+            //account2.AccountNumber = 45566666;
+            //account2.Balance = 1000000000000000000; 
+            account2.Deposit(150.00);
+            Console.WriteLine("Account Name: {0}, Number: {1}, Type of Account: {2}, Balance: {3:c}", account2.AccountName,
+                account2.AccountNumber, account2.TypeOfAccount, account2.Balance);
+
+        }
+    }
+}
